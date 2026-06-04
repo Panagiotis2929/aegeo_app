1882208c4696c736da6a005802826e2565088b10
Panagiotis2929 <rpkyranas@gmail.com>
2026-06-05 00:32:40 +0300

    Fix departure city bug and add modern UI animations

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/aegeo_app/', // <--- Πρόσθεσε αυτή τη γραμμή ακριβώς
   server: {
     port: 3000
   }
