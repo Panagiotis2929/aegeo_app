ecc91a8fc5b9ed5ff590e98cd89be2a23fbf5ba7
Panagiotis2929 <rpkyranas@gmail.com>
2026-06-05 02:03:56 +0300

    Fix base path for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: './', // ΑΥΤΟ ΕΙΝΑΙ ΤΟ ΚΛΕΙΔΙ!
   plugins: [react()],
-  base: '/aegeo_app/',
-  server: {
-    port: 3000
-  }
-});
+})
